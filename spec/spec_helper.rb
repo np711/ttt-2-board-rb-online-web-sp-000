e7750f143817f745019642d7c5f8e634e85855fd
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-11-03 19:21:15 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,14 @@
-RSpec.configure do |config|
-  config.order = :default
+def capture_puts
+  begin
+    old_stdout = $stdout
+    $stdout = StringIO.new('','w')
+    yield
+    $stdout.string
+  ensure
+    $stdout = old_stdout
+  end
 end
 
-# Ignore this.
-def get_variable_from_file(file, variable)
-  file_scope = binding
-  file_scope.eval(File.read(file))
-  
-  begin
-    return file_scope.local_variable_get(variable)
-  rescue NameError
-    raise NameError, "local variable `#{variable}' not defined in #{file}."
-  end    
+RSpec.configure do |config|
+  config.order = :default
 end
