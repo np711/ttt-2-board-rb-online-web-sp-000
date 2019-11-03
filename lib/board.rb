1e9ea21b0890242638d85b8179ea2f88b6f53d01
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-11-03 19:21:38 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,15 @@
 # Define the variable board below.
+
+board = [" "," "," "," "," "," "," "," "," "]
+[" "]
+["X"]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board[9] = "X")
